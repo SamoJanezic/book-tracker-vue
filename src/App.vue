a9eccf3ad07bef066fdd74dbbeaae6fcee99f2a3
samo <janezic.orion@gmail.com>
2023-01-14 18:03:52 +0100

    added flex-box and button styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/book-list">Book List</router-link> | 
-    <router-link to="/add-book">Add Book</router-link>
-  </nav>
+    <nav class="flex-container">
+      <router-link class="link-btn" to="/">Home</router-link>
+      <router-link class="link-btn" to="/book-list">Book List</router-link>
+      <router-link class="link-btn" to="/add-book">Add Book</router-link>
+    </nav>
   <router-view/>
 </template>
 
@@ -26,6 +26,31 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #000000;
 }
+
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  /* background-color: rgb(205, 233, 242); */
+  align-items: flex-end;
+  padding-bottom: 12px;
+  background-color: rgb(0, 0, 0);
+}
+
+.link-btn {
+  padding: 5px 20px;
+  margin: 0px 18px;
+  background-color: rgba(255, 240, 240, 0.633);
+  border: 2px solid black;
+  border-radius: 0px 0px;
+  transition: 350ms ease-in-out;
+}
+
+.link-btn:hover {
+  /* border-radius: 20px 0px; */
+  color: blue;
+  background-color: lightblue;
+}
+
 </style>
