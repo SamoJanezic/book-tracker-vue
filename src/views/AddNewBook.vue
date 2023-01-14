88ff5295b96918fd53a7baa690eb592bb2ad57a5
samo <janezic.orion@gmail.com>
2023-01-14 18:11:25 +0100

    added style to AddNewBook

--- src/views/AddNewBook.vue
+++ src/views/AddNewBook.vue
@@ -1,22 +1,23 @@
 <template>
+  
 	<h3>Here you can add a new book</h3>
-  <form class="grid-container">
-		<label class="grid-item grid-item-1">Title: </label>
+  <form class="form-container">
+		<label class="item">Title: </label>
 		<input type="text" v-model="book.title" /> <br>
 
-    <label class="grid-item grid-item-1">Author: </label>
+    <label class="item">Author: </label>
     <input type="text" v-model="book.author" /><br>
 
-    <label class="grid-item grid-item-1">Pages: </label>
+    <label class="item">Pages: </label>
     <input type="text" v-model="book.pages" /><br>
 
-    <label class="grid-item grid-item-1">Publisher: </label>
+    <label class="item">Publisher: </label>
     <input type="text" v-model="book.publisher" /><br>
 
-    <label class="grid-item grid-item-1">Publication year: </label>
+    <label class="item">Publication year: </label>
     <input type="text" v-model="book.publicationYear" /><br>
-    
-    <label class="grid-item grid-item-1">Image url: </label>
+
+    <label class="item">Image url: </label>
     <input type="text" v-model="book.image" />
 	</form>
   <button @click="showInput">Add Book</button>
@@ -24,6 +25,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import './AddNewBook.css';
 
 export default defineComponent({
 	name: 'addBook',
@@ -41,8 +43,9 @@ export default defineComponent({
 	},
   methods: {
     showInput() {
-      console.log(this.book);
+      console.log(this.book)
     },
+
     // addBook() {
 
     // }
